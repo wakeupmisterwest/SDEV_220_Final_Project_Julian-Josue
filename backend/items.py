68931ea41dcf90a00c23685859c513d40da26287
aaryak1231 <abashyal@ivytech.edu>
2025-11-22 23:42:05 -0500

    this is basic items.py

---
+++ backend/items.py
@@ -0,0 +1,17 @@
+
+#This is item Class and it Represents an individual item food or drink in the inventory
+
+class Item:
+    #Represents an individual item food or drink
+    def __init__(self, itemID: int, name: str, description: str, price: float):
+   
+        self.itemID = itemID # stores the unique id for the item
+        self.name = name    # this stores name of the item
+        self.description = description  # this stores description about the items
+        self.price = price # this stores price of the items
+    
+    def __str__(self):
+        return f"Item({self.itemID}, {self.name}, ${self.price:.2f})"
+    
+    def __repr__(self):
+        return self.__str__()
